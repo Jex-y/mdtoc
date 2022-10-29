1f77d3ce009076dc8bda70f40093f046abf066ad
Edward Jex <edwardjex@live.co.uk>
2022-10-29 21:35:51 +0100

    fix: links again

--- index.js
+++ index.js
@@ -17,27 +17,6 @@ const generateToc = (fileData) => {
             depth++;
         }
 
-        title = title.replace(/(\d\.)*\d /, "");
-
-        const newTitle = `${repeateString("#", depth)} ${indexes
-            .slice(1)
-            .join(".")} ${title.slice(depth + 1)}`;
-
-        const id = newTitle
-            .toLowerCase()
-            .slice(depth + 1)
-            .trim()
-            .replace(/ /g, "-")
-            .replace(/[^a-z0-9-]/g, "")
-            .replace(/-+/g, "-");
-
-        if (IDs[id]) {
-            IDs[id]++;
-            title += `-${IDs[id]}`;
-        } else {
-            IDs[id] = 1;
-        }
-
         if (depth > prevDepth) {
             indexes.push(1);
         } else if (depth < prevDepth) {
@@ -47,18 +26,37 @@ const generateToc = (fileData) => {
             indexes[indexes.length - 1]++;
         }
 
-        prevDepth = depth;
         const number = indexes.slice(1).join(".");
+        const newTitle = `${repeateString("#", depth)} ${
+            number ? number + " " : ""
+        }${title.replace(/(\d\.)*\d /, "").slice(depth + 1)}`;
 
-        toc += `${repeateString(" ", (depth - 1) * 2)}- [${number}${
-            number ? " " : ""
-        }${title.slice(depth + 1)}](#${id})\n`;
+        const id = newTitle
+            .toLowerCase()
+            .slice(depth + 1)
+            .replace(/[^a-z0-9- ]/g, "")
+            .trim()
+            .replace(/ /g, "-")
+            .replace(/-+/g, "-");
+
+        if (IDs[id]) {
+            IDs[id]++;
+            newTitle += `-${IDs[id]}`;
+        } else {
+            IDs[id] = 1;
+        }
+
+        prevDepth = depth;
+
+        toc += `${repeateString(
+            " ",
+            (depth - 1) * 2
+        )}- [${newTitle}](#${id})\n`;
 
         fileData = fileData.replace(title, newTitle);
     });
 
     const output = fileData.replace(tocRegex, `$1\n${toc}$3`);
-    // console.log(output);
     return output;
 };
 
@@ -70,24 +68,27 @@ const repeateString = (string, times) => {
     return toc;
 };
 
-const globs = core.getInput("files").split(" ");
+// const globs = core.getInput("files").split(" ");
 
-globs.forEach((globPath) => {
-    glob(globPath, (err, files) => {
-        if (err) {
-            core.setFailed(err.message);
-        }
-        files.forEach((file) => {
-            fs.readFile(file, "utf8", (err, data) => {
-                if (err) {
-                    core.setFailed(err.message);
-                }
-                fs.writeFile(file, generateToc(data), (err) => {
-                    if (err) {
-                        core.setFailed(err.message);
-                    }
-                });
-            });
-        });
-    });
-});
+// globs.forEach((globPath) => {
+//     glob(globPath, (err, files) => {
+//         if (err) {
+//             core.setFailed(err.message);
+//         }
+//         files.forEach((file) => {
+//             fs.readFile(file, "utf8", (err, data) => {
+//                 if (err) {
+//                     core.setFailed(err.message);
+//                 }
+//                 fs.writeFile(file, generateToc(data), (err) => {
+//                     if (err) {
+//                         core.setFailed(err.message);
+//                     }
+//                 });
+//             });
+//         });
+//     });
+// });
+
+const fileData = fs.readFileSync("file.md", "utf8");
+generateToc(fileData);
